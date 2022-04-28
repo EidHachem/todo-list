aa4d2b35a1098eb9e17e6ec81c7a6a1490129ea9
Eid Hachem <eidhachem1@gmail.com>
2022-04-28 16:19:12 +0300

    Apply es6 modules by addind add.js and edit.js

---
+++ src/modules/edit.js
@@ -0,0 +1,53 @@
+let x = JSON.parse(localStorage.getItem('savedData'));
+export let tasks = [];
+
+if (x) {
+  tasks = x;
+}
+
+export default function edit(e) {
+  const item = e.target;
+
+  if (item.classList.contains('fa-trash-can')) {
+    const removeParent = item.parentElement.parentElement;
+    removeParent.remove();
+
+    const removeTaskFromArray =
+      item.parentElement.previousElementSibling.children[0].getAttribute(
+        'name'
+      );
+
+    tasks = tasks
+      .sort((a, b) => {
+        return a.index - b.index;
+      })
+      .filter((newTask) => {
+        if (newTask.index !== +removeTaskFromArray) {
+          return true;
+        }
+      })
+      .sort((task, newTask) => {
+        if (task.index + 1 === newTask.index) {
+          return;
+        } else {
+          newTask.index = task.index + 1;
+        }
+      });
+
+    localStorage.setItem('savedData', JSON.stringify(tasks));
+    console.log(tasks);
+  }
+
+  if (item.classList.contains('fa-ellipsis-vertical')) {
+    item.nextElementSibling.style.display = 'block';
+    item.style.display = 'none';
+  }
+
+  if (item.classList.contains('id')) {
+    item.nextElementSibling.classList.toggle('line-through');
+  }
+
+  if (item.classList.contains('user-input')) {
+    item.removeAttribute('readonly');
+  }
+}
